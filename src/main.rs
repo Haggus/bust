eb65bc418c6244cd7d9da4c7b9b3efb71cc0018b
Mateusz Mrowiec <matt.mrowiec@gmail.com>
2017-12-04 19:09:27 +0100

    Add info banner

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
 fn main() {
+    println!(" ____               _   ");
+    println!("|  _ \\             | |  ");
+    println!("| |_) | _   _  ___ | |_ ");
+    println!("|  _ < | | | |/ __|| __|");
+    println!("| |_) || |_| |\\__ \\| |_ ");
+    println!("|____/  \\__,_||___/ \\__|\n");
+    println!("Brainfuck REPL");
+    println!("Written by Mateusz 'Haggus' Mrowiec\n");
+    println!("Press Ctrl+C to Exit\n");
+
     let mut rl = Editor::<()>::new();
 
     loop {
