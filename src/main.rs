a9a3d8e57a96d1e92fc4b4e660bbec14c876515f
Mateusz Mrowiec <matt.mrowiec@gmail.com>
2017-12-04 18:52:54 +0100

    Add simple read loop using rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rl = Editor::<()>::new();
+
+    loop {
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(line) => {
+                println!("{}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
 }
