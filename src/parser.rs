fbfb409249b360200e4e3cba1f7b5bb370585ee5
Mateusz Mrowiec <matt.mrowiec@gmail.com>
2017-12-05 17:44:35 +0100

    Update parse to return instructions

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,25 @@
+use vm::Instruction;
+
 pub struct Parser {}
 
 impl Parser {
-    pub fn parse(source: &str) -> Vec<char> {
+    pub fn parse(source: &str) -> Vec<Instruction> {
         source
             .chars()
             .filter(|&c| match c {
                 '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']' => true,
                 _ => false,
+            })
+            .map(|c| match c {
+                '>' => Instruction::MoveRight,
+                '<' => Instruction::MoveLeft,
+                '+' => Instruction::Increment,
+                '-' => Instruction::Decrement,
+                '.' => Instruction::Output,
+                ',' => Instruction::Input,
+                '[' => Instruction::JumpRight(0),
+                ']' => Instruction::JumpLeft(0),
+                _ => panic!("Unknown instruction"),
             })
             .collect()
     }
@@ -17,10 +30,18 @@ mod tests {
     use super::*;
 
     #[test]
+    fn translates_chars_to_instructions() {
+        assert_eq!(
+            Parser::parse("+++"),
+            vec![Instruction::Increment, Instruction::Increment, Instruction::Increment]
+        );
+    }
+    #[test]
+
     fn filters_out_unnecessary_characters() {
         assert_eq!(
             Parser::parse("><hello..[]"),
-            vec!['>', '<', '.', '.', '[', ']']
+            vec![Instruction::MoveRight, Instruction::MoveLeft, Instruction::Output, Instruction::Output, Instruction::JumpRight(0), Instruction::JumpLeft(0)]
         );
     }
 }
